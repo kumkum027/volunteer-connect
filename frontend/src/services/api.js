6a81414fb1a1f88c56c161ea710bb7e30ad735ce
kumkum194 <kumkumg194@gmail.com>
2026-07-05 21:22:05 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // local backend
+  baseURL: 'https://volunteer-connect-oe3h.onrender.com/api', // local backend
   headers: {
     'Content-Type': 'application/json',
   },
